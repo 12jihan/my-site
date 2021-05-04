08c2383ff14e8f27fcea59f34f14d80f1beeefbe
Jareem <Jareem.X.Hoff.-ND@abc.com>
2021-05-04 09:09:58 -0400

    finally was able to get the particles working properly

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,49 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import TypewriterComponent from '../components/TypewriterComponent';
+import React, { useEffect } from 'react';
+import Particles from 'react-particles-js';
+import Particle_Config from '../assets/particles';
 
 export default function Home() {
   const titles = [
     'Software Engineer',
     'Content Creator',
     'Tech Enthusiast'
-  ]
+  ]  
+
+  const canvas_style = {
+    backgroundColor: 'black'
+  }
+  
+  useEffect(() => {
+    console.log(Particle_Config);
+  }, []);
 
   return (
     <>
-      <div className="hero has-background-dark is-medium is-primary hero-header">
+      {/* <div className="hero has-background-dark is-medium is-primary hero-header">
         <div className="hero-body">
           <p className="title has-text-centered has-text-weight-light is-size-1">
             Jareem E. Hoff
           </p>
           <TypewriterComponent text={titles} />
         </div>
+      </div> */}
+
+      <div className="container">
+        <Particles 
+          params={Particle_Config} 
+          // height="500px" 
+          // width="500px" 
+          style={canvas_style}
+        />
       </div>
 
-      <div className="container mt-6">
-        <div className="columns">
+      {/* <div className="container mt-6">
+        <div className="columns is-multiline">
           
-          <div className="column is-6">
+          <div className="column is-12">
             <div className="columns is-multiline is-centered">    
               <div className="column is-12">
                 <h1 className="title has-text-weight-medium">About Jareem</h1>
@@ -48,13 +68,13 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <style jsx>{`
         .hero-header {
-          background-image: url("https://coolbackgrounds.io/images/backgrounds/index/ranger-4df6c1b6.png");
-          background-repeat: no-repeat;
-          background-size: cover;
+          // background-image: url("https://coolbackgrounds.io/images/backgrounds/index/ranger-4df6c1b6.png");
+          // background-repeat: no-repeat;
+          // background-size: cover;
         }
       `}
       </style>
