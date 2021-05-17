2f880a60d673b28dff4ab90e735daac33515147b
Jareem <Jareem.X.Hoff.-ND@abc.com>
2021-05-17 03:21:55 -0400

    working on the github component at the moment

--- components/GithubHomeComponent.js
+++ components/GithubHomeComponent.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function GithubHomeComponent(props) {
+    const git_data = props.git;
+
+    useEffect(() => {
+        console.log("git props: ", git_data);
+    }, [])
+
     return (
-        <div>
-            Hi! This is the component that I use to show the projects I'm working on.
-        </div>
+        <>
+
+            <div className="table-container">
+                <div className="container-fluid">
+                    <div className="columns is-multiline ">
+                        {
+                            git_data.map((item) => {
+                                console.log(item.name);
+
+                                return (
+                                    <div className="column is-12">
+                                        <div className="columns">
+                                            <div className="column is-1 has-text-centered">
+                                                <a href={`https://github.com/12jikan/${item.name}/archive/refs/heads/${item.default_branch}.zip`}>
+                                                    <FontAwesomeIcon 
+                                                        icon={faArrowCircleDown} 
+                                                    />
+                                                </a>
+                                            </div>
+
+                                            <div className="column is-3">
+                                                    <p>{item.name}</p>
+                                            </div>
+
+                                            <div className="column is-3">
+                                            </div>
+
+                                            <div className="column is-3">
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+
+            <style jsx>{`
+                .table-container {
+                    position: relative;
+                    width: auto;
+                    height: 300px;
+                    overflow-x: hidden;
+                    overflow-y: scroll;
+                };
+            `}</style>
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
 import TypewriterComponent from '../components/TypewriterComponent';
 import React, { useEffect } from 'react';
-import Particles from 'react-particles-js';
-import Particle_Config from '../assets/particles';
+// import Particles from 'react-particles-js';
+// import Particle_Config from '../assets/particles';
 import GithubHomeComponent from '../components/GithubHomeComponent';
+import axios from 'axios';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await axios.get('https://api.github.com/users/12jikan/repos');
+  const data = await res.data
+
+  return {
+    props: {
+      gitProfile: data
+    }
+  }
+};
+
+export default function Home(props) {
   const titles = [
     'Software Engineer',
     'Content Creator',
     'Tech Enthusiast'
   ];
 
+  let github_data = props.gitProfile;
+
   const darkColor = '#363636';
   const lightColor = '#e0e0e0';
 
-  // const canvas_style = {
-  //   backgroundColor: 'black'
-  // };
-
   {/* <TypewriterComponent text={titles} /> */ }
+
   {/* <div className="container">
         <Particles 
           params={Particle_Config} 
@@ -28,36 +39,6 @@ export default function Home() {
           // width="500px" 
           style={canvas_style}
         />
-      </div> */}
-
-  {/* <div className="container mt-6">
-        <div className="columns is-multiline">
-          
-          <div className="column is-12">
-            <div className="columns is-multiline is-centered">    
-              <div className="column is-12">
-                <h1 className="title has-text-weight-medium">About Jareem</h1>
-              </div>
-
-              <div className="column is-12">
-                <p className="is-size-5">Jareem Hoff is a web and software engineer based out of New York City, New York. He is currently working as a contract developer at ABC News. In his spare time he tries to further build up his skills as a software engineer, by planning out future applications that come to mind or working on personal projects all while reading up on new technologies and systems. Even though he spends most of his time as a web developer, he does work in other languages such as Java, Python, C++ and Golang, to try and understand programming from a deeper level. His pronouns are he/him.</p>
-              </div>
-
-            </div>
-          </div>
-
-          <div className="column is-6">
-            <div className="columns is-multiline is-centered">
-              <div className="column is-12">
-                <h1 className="title has-text-weight-medium">Current Projects</h1>
-              </div>
-
-              <div className="column is-12">
-                (git hub stuff here)
-              </div>
-            </div>
-          </div>
-        </div>
       </div> */}
 
   return (
@@ -94,8 +75,8 @@ export default function Home() {
                   <h1 className="">Projects</h1>
                 </div>
 
-                <div>
-                  
+                <div className="column is-12">
+                  <GithubHomeComponent  git={ github_data }/> 
                 </div>
               </div>
             </div>
